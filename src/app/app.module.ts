9ff964543a6d81fb058402e054e94b99a4e301ee
Jose Luis Muñoz <joseluismunozzuniga@gmail.com>
2022-04-25 00:50:00 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarModule } from '@shared/navbar/navbar.module';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
